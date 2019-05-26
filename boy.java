17470d2b4029eaa18853667a10f7fabb9ac2b82e
Felipe Queiroz <felipef.queiroz@hotmail.com>
2019-05-26 14:54:05 -0300

    Add files via upload

--- boy.java
+++ boy.java
@@ -1,12 +1,18 @@
 public class boy extends Pessoa{
-  private double desconto;
+  private double desconto, previdencia;
   
   public boy (int ID, String nome, int departamento, double salario, double adicional){
 	super(ID, nome, departamento, salario);
+	this.previdencia=0.05; // Desconto fixo de um sistema de previd�ncia da empresa
     this.desconto =0.02;
   }
 
   void setDesconto(double desconto){
     this.desconto=desconto;
   }
+  
+  double pagamento(){
+	    // Calcula o salário líquido
+	  return(salario*(1- this.previdencia-this.desconto));
+	  }
 }
--- funcionario.java
+++ funcionario.java
@@ -2,7 +2,7 @@ public class funcionario extends Pessoa{
   private double desconto, adicional, previdencia;
 
   public funcionario(int ID, String nome, int departamento, double salario, double adicional){
-    super(ID, nome, departamento, salario);
+    super(ID, nome, departamento, salario);;
     this.desconto=0.02;
     this.previdencia=0.05; // Desconto fixo de um sistema de previd�ncia da empresa
     this. adicional = adicional; // adicional caso o funcionário seja por exemplo chefe.
@@ -16,9 +16,10 @@ public class funcionario extends Pessoa{
     this.adicional = adicional*(1+novo_percentual);
   }
   
+  
  @Override
   double pagamento(){
     // Calcula o salário líquido
-    return(this.salario*(1- this.previdencia- this.desconto+ this.adicional));
+    return(salario*(1- this.previdencia- this.desconto+ this.adicional));
   }
 }
